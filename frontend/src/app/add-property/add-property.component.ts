a108a32c6e774d623d5ac49702c377ec2e775934
Vishvajit Kher <vishvajit79@gmail.com>
2018-05-05 17:55:04 -0400

    form validation added and post request after submitting the form

--- frontend/src/app/add-property/add-property.component.ts
+++ frontend/src/app/add-property/add-property.component.ts
@@ -43,6 +43,7 @@ export class AddPropertyComponent implements OnInit {
   createProperty(value) {
     console.log(value);
     this.propertyService.createProperty(value);
+    this.addPropertyForm.reset();
   }
 
   createItem(): FormGroup {
